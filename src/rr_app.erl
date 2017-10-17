5d558943b8697dd75f9cd3eacdece4fb743c1397
Roman Galeev <roman.galeev@ezuce.com>
2017-10-17 16:06:34 +0200

    Update to Cowboy 2.0

--- src/rr_app.erl
+++ src/rr_app.erl
@@ -1,20 +1,20 @@
 -module(rr_app).
 -behaviour(application).
 
--export([start/0, start/2, stop/1]).
+-export([start/2, stop/1]).
+
+dispatch_rules() ->
+	cowboy_router:compile([
+		{'_', [
+			{'_', rr_handler, []}
+		]}
+	]).
 
 start(_Type, _Args) ->
-	Dispatch = cowboy_router:compile([
-		{'_', [{'_', rr_handler, []}]}
-	]),
-	cowboy:start_http(rr_http_listener, 100, [{port, 9090}],
-		[{env, [{dispatch, Dispatch}]}]
+	cowboy:start_clear(rr_http_listener,
+		[{port, 9090}],
+		#{env => #{dispatch => dispatch_rules()}}
 	).
-
-start() ->
-	application:start(lager),
-	application:start(cowboy),
-	ok.
 
 stop(_State) ->
 	ok.
--- src/rr_handler.erl
+++ src/rr_handler.erl
@@ -1,52 +1,44 @@
 -module(rr_handler).
--export([init/3, handle/2, terminate/3]).
+-export([init/2]).
 
-init({_, http}, Req, _Opts) ->
-	{ok, Req, undefined}.
-
-handle(Req, State) ->
-	{Method, _} = cowboy_req:method(Req),
-	try handle_method(Method, Req) of
+init(#{ method := Method, path := Path }=Req0, State) ->
+	try handle_method(Method, Req0) of
 		{ok, Re} -> {ok, Re, State}
 	catch
 		C:E ->
-			{Path, _} = cowboy_req:path(Req),
 			lager:error("~p:~p path:~p ~p", [C, E, Path, erlang:get_stacktrace()]),
-			{ok, Re} = reply(404, <<"not_found">>, Req),
+			{ok, Re} = reply(404, <<"not_found">>, Req0),
 			{ok, Re, State}
 	end.
-
-terminate(_Reason,_Req, _State) ->
-	ok.
 
 reply(Code, Req) ->
 	reply(Code, <<"ok">>, Req).
 reply(Code, Msg, Req) ->
-	cowboy_req:reply(Code, [{<<"connection">>, <<"close">>}, {<<"content-type">>, <<"text/plain">>}], Msg, Req).
+	cowboy_req:reply(Code, #{ <<"connection">> => <<"close">>, <<"content-type">> => <<"text/plain">>}, Msg, Req).
 
-handle_method(<<"PUT">>, Req) ->
-	maybe_write(Req, cowboy_req:path(Req));
-handle_method(<<"GET">>, Req) ->
-	read(cowboy_req:path(Req)).
+handle_method(<<"PUT">>, #{ path := Path}=Req) ->
+	maybe_write(Path, Req);
+handle_method(<<"GET">>, #{ path := Path }=Req) ->
+	read(Path, Req).
 
-maybe_write(Req, {<<"/", Path/binary>>, _}=P) ->
+maybe_write(<<"/", Path/binary>>=P, Req) ->
 	case filelib:is_file(Path) of
 		true ->
 			reply(409, <<"file_exists">>, Req);
 		false ->
-			write(cowboy_req:body(Req), P)
+			write(cowboy_req:read_body(Req), P)
 	end.
 
-write(_, {<<"/">>, Req}) -> reply(403, <<"path required">>, Req);
-write({ok, Data, Req}, {<<"/", Path/binary>>, _}) ->
-	lager:info("write file:~p size:~p", [Path, get_size(cowboy_req:body_length(Req))]),
+write(<<"/">>, Req) -> reply(403, <<"path required">>, Req);
+write({ok, Data, Req},  <<"/", Path/binary>>) ->
+	lager:info("write file:~p size:~p", [Path, cowboy_req:body_length(Req)]),
 	ensure_folder(filename:dirname(Path)),
 	ok = file:write_file(Path, Data),
-	reply(200, Req);
-write({more, Data, Req}, {<<"/", Path/binary>>, _}) ->
-	lager:info("write file:~p size:~p", [Path, get_size(cowboy_req:body_length(Req))]),
+	{ok, reply(200, Req)};
+write({more, Data, Req}, <<"/", Path/binary>>) ->
+	lager:info("write file:~p size:~p", [Path, cowboy_req:body_length(Req)]),
 	ensure_folder(filename:dirname(Path)),
-	reply(200, write_append(Path, {more, Data, Req})).
+	{ok, reply(200, write_append(Path, {more, Data, Req}))}.
 
 write_append(Path, {ok, Data, Req}) ->
 	ok = file:write_file(Path, Data, [append]),
@@ -55,11 +47,9 @@ write_append(Path, {more, Data, Req}) ->
 	ok = file:write_file(Path, Data, [append]),
 	write_append(Path, cowboy_req:body(Req)).
 
-read({<<"/", Path/binary>>, Req}) ->
+read(<<"/", Path/binary>>, Req) ->
 	{ok, Binary} = file:read_file(Path),
 	cowboy_req:reply(200, [{<<"content-length">>, erlang:integer_to_binary(erlang:size(Binary))}], Binary, Req).
-
-get_size({Size, _}) -> Size.
 
 ensure_folder(Dir) ->
 	ok = filelib:ensure_dir(<<Dir/binary, "/">>).
