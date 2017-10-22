97140dd0db979e0020e63ff6a873d889c9a107ee
Roman Galeev <roman.galeev@ezuce.com>
2017-10-22 23:29:56 +0200

    Fix shoutcast header sequence

--- src/rr_shout_handler.erl
+++ src/rr_shout_handler.erl
@@ -1,7 +1,7 @@
 -module(rr_shout_handler).
 -export([loop/1]).
 
--define(CHUNK_SIZE, 4096).
+-define(CHUNK_SIZE, 96).
 
 -record(state, {
 	uri
@@ -25,12 +25,12 @@ response() -> [
 	"ICY 200 OK\r\n",
 	"content-type: audio/mpeg\r\n",
 	"icy-pub: 1\r\n",
-%	"icy-metaint: ",integer_to_list(?CHUNK_SIZE),"\r\n",
+	"icy-metaint: ",integer_to_list(?CHUNK_SIZE),"\r\n",
 	"\r\n"
 ].
 
 shout_header() ->
-	shout_header(<<"StreamTitle='Stream';">>).
+	shout_header(<<"StreamTitle='H';">>).
 
 shout_header(Info) ->
 	Nblocks = ((size(Info) - 1) div 16) + 1,
@@ -50,17 +50,18 @@ stream_file(Socket, Fh, Offset, Tail) ->
 	ChunkSize = ?CHUNK_SIZE - byte_size(Tail),
 	case file:pread(Fh, Offset, ChunkSize) of
 		{ok, FileData} when byte_size(<<Tail/binary, FileData/binary>>) =:= ?CHUNK_SIZE ->
-			ok = gen_tcp:send(Socket, [Tail, FileData]),
+			ok = gen_tcp:send(Socket, [Tail, FileData, shout_header()]),
 			stream_file(Socket, Fh, Offset+ChunkSize, <<>>);
 		{ok, FileData} ->
 			stream_file(Socket, Fh, 0, <<Tail/binary, FileData/binary>>);
 		eof when Offset =:= 0, byte_size(Tail) =:= 0 ->
 			lager:notice("zero file");
 		eof when byte_size(Tail) =:= ?CHUNK_SIZE ->
-			ok = gen_tcp:send(Socket, [Tail]),
+			ok = gen_tcp:send(Socket, [Tail, shout_header()]),
 			stream_file(Socket, Fh, 0, <<>>);
 		eof ->
 			stream_file(Socket, Fh, 0, Tail);
 		{error, Reason} ->
 			lager:notice("file error:~p", [Reason])
 	end.
+
